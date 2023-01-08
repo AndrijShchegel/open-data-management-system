69575d9b6650c28131d2cd3822df70ccb4a884f4
AndrijShchegel <89912205+AndrijShchegel@users.noreply.github.com>
2023-01-08 02:08:01 +0200

    add source files

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 5000;
+const host = '0.0.0.0';
+
+const app = express();
+
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/user', require('./controls'));
+
+app.listen(port, host, () => {
+  console.log(`Server started: ${host}:${port}`);
+});
